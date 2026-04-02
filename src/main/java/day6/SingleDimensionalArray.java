4df403c49abeb1b1039a971d352e7a62989064e5
Rahulugale14 <ugalerahulofficial@gmail.com>
2026-04-02 10:55:30 +0530

    Day 06 - Arrays

---
+++ src/main/java/day6/SingleDimensionalArray.java
@@ -0,0 +1,35 @@
+package day6;
+
+public class SingleDimensionalArray {
+
+    /*
+    1) Declare an array             arrays always start from zer i.e. index always start from zero
+    2) Add values into array
+    3) Find size of an array
+    4) Read single value from array
+    5) Read multiple values from an array
+     */
+
+        public static void main (String [] args) {
+
+            // DECLARING ARRAY
+
+            // 1st Approach
+            int a[]=new int[5];
+            a[0]=100;
+            a[1]=200;
+            a[2]=300;
+            a[3]=400;
+            a[4]=500;
+
+            // 2nd Approach                 assigning in one single line
+            int b [] = {100,200,300,400,500};
+
+
+
+
+
+
+        }
+
+}
